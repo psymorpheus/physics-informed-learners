799aa81156533e50227fc755fc5faf4b5b42e0d6
mayank0926 <yadav.mayank2000@gmail.com>
2021-09-05 15:56:54 +0530

    Better data collection

--- collect-original-data.py
+++ collect-original-data.py
@@ -2,31 +2,34 @@ import gym
 import mujoco_py
 import numpy as np
 import scipy.io as sc
+import time
 
 env = gym.make('FetchSlide-v1')
 env.reset()
 iter = 0
 
 defStep = np.array([-1,-1,1,0],dtype=np.float32)
-pre_obs, reward, done, info = env.step(defStep)
-data = np.array([np.zeros(8, dtype=np.float32)])
+obs = env.reset()
 
-while (iter<500):
+obj_pos = obs["observation"][3:6]
+obj_vel = obs["observation"][14:17]
+data = np.array([np.hstack([[0],obj_pos,obj_vel,[obj_vel[1]/obj_vel[0]]])])
+
+while (iter<200):
     env.render(mode="human")
     obs, reward, done, info = env.step(defStep)
     # print(obs["observation"][:3] - pre_obs["observation"][:3], "-----", obs["observation"][-6:-3])
-    if iter>50 and iter<80: defStep[:3] = 50*obs["observation"][6:9]
-
+    # if iter>50 and iter<80: defStep[:3] = 50*obs["observation"][6:9]
 
     obj_pos = obs["observation"][3:6]
     obj_vel = obs["observation"][14:17]
 
-    pre_obs = obs
     iter += 1
 
     newrow = np.array([np.hstack([[iter],obj_pos,obj_vel,[obj_vel[1]/obj_vel[0]]])])
     data = np.vstack([data, newrow])
 
-np.savetxt("data1.csv", data, delimiter=",")
-sc.savemat("data1.mat", {'data':data})
+filename = "data-" + time.strftime("%Y%m%d-%H%M%S")
+np.savetxt(filename + ".csv", data, delimiter=",")
+sc.savemat(filename + ".mat", {'data':data})
 env.close()
